9884fe22e727e099d1f4b0852a15939e26b4ca9e
AndersonPiva <andersopiva@hotmail.com>
2016-10-08 11:19:27 -0300

    commits

--- app/controllers/publics_controller.rb
+++ app/controllers/publics_controller.rb
@@ -2,7 +2,9 @@ class PublicsController < ApplicationController
 
   def index
     @total_anuncios = Advertisement.all.size
-    @cities = City.all.order(name: :asc)
+    @cities_concordia = City.where(microregion: 'concordia').order(name: :asc)
+    @cities_chapeco = City.where(microregion: 'chapeco').order(name: :asc)
+    @cities_xanxere = City.where(microregion: 'xanxere').order(name: :asc)
   end
 
   def privacy
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160614030509) do
+ActiveRecord::Schema.define(version: 20161007163130) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -33,6 +33,7 @@ ActiveRecord::Schema.define(version: 20160614030509) do
 
   create_table "cities", force: :cascade do |t|
     t.string "name"
+    t.string "microregion"
   end
 
   create_table "evaluations", force: :cascade do |t|
